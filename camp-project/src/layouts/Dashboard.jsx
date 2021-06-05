8f013c99fcae543d8f45a979f4c57a3a52a5e275
miracthis <mirac@matabt.com>
2021-06-06 00:54:24 +0300

    D11/LL

--- camp-project/src/layouts/Dashboard.jsx
+++ camp-project/src/layouts/Dashboard.jsx
@@ -1,15 +1,22 @@
 import React from "react";
+import { Grid } from "semantic-ui-react";
 import ProductList from "../pages/ProductList";
 import Categories from "./Categories";
-import Navi from "./Navi";
 
 //**rfc Yazarsan otomatik oluşturur */
 export default function Dashboard() {
   return (
     <div>
-        <Navi/>
-        <Categories/>
-        <ProductList/>
+      <Grid>
+      <Grid.Row >
+        <Grid.Column width={4}>
+          <Categories />
+        </Grid.Column>
+        <Grid.Column width={12}>
+          <ProductList />
+        </Grid.Column>
+      </Grid.Row>
+      </Grid>
     </div>
   );
 }
